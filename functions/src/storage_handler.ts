86f1e0da1a7acd7af0071bc5aa9bc2aa533713da
veysel ozkahraman <vzkahraman3@gmail.com>
2021-04-18 22:55:41 +0300

    async fix

--- functions/src/storage_handler.ts
+++ functions/src/storage_handler.ts
@@ -3,16 +3,18 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp(functions.config().firebase);
 
-export const getFile = (): unknown => {
+export const getFile = async (): Promise<unknown> => {
   try {
     let file = {};
-    admin.storage().bucket().file("conf.json")
+    return admin.storage().bucket().file("conf.json")
         .download(function(err, contents) {
           if (!err) {
             file = JSON.parse(contents.toString("utf8"));
+            return file;
+          } else {
+            return {err};
           }
         });
-    return Object.assign({}, file);
   } catch (error) {
     return {error};
   }
